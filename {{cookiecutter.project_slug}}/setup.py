ccfd99e2f270f7651c23f90106fe110e86bd4a47
Mario Costa <you@example.com>
2019-03-08 11:22:38 +0000

    integrated python into the project including setup.py test.

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -10,23 +10,20 @@ from setuptools import find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-requirements = [] # TODO: load from conda file.
+requirements = [] # TODO: load from conda file ?
 
 setup_requirements = [ ]
 
 test_requirements = [ ]
 
-# Require pytest-runner only when running tests
-pytest_runner = (['pytest-runner>=2.0,<3dev']
-                 if any(arg in sys.argv for arg in ('pytest', 'test'))
-                 else [])
-
-setup_requires = pytest_runner
-
 
 # 1.
 # if environment CONDA_BUILD=1, then we are building inside conda, so we need to
 # set proper library locations and options. see https://conda.io/projects/conda-build/en/latest/source/environment-variables.html
+# to build for a conda environment please check the environment variables
+# https://docs.conda.io/projects/conda-build/en/latest/source/environment-variables.html?highlight=environment
+#        '-DCMAKE_INSTALL_PREFIX={}'.format(os.environ['PREFIX']),
+#        '-DPYTHON_SITE_PACKAGES={}'.format(os.environ['SP_DIR']),
 
 # 2.
 # if under an active conda environment, check for one of the following env 
@@ -44,13 +41,20 @@ setup_requires = pytest_runner
 cmake_args = [
     '-DBUILD_PYTHON_PYBIND11=ON',
 #    '-DBUILD_PYTHON_SWIG=ON',
-    '-DINSTALL_FOR_PYPI=ON',
 ]
 
-if os.environ.get('CONDA_BUILD') == '1': # if under conda build
+
+if os.environ.get('CONDA_BUILD') == '1':
+# if under conda build, install for the conda environment, spliting the libs
+
     cmake_args += [
         '-DCMAKE_INSTALL_PREFIX={}'.format(os.environ['PREFIX']),
     ]
+else:
+    cmake_args += [
+        '-DINSTALL_FOR_PYPI=ON',
+    ]
+
 
 
 # see namespace packages https://packaging.python.org/guides/packaging-namespace-packages/#native-namespace-packages
@@ -65,20 +69,9 @@ setup(
     package_dir = {'': os.path.join('src','python')},
     packages=['{{ cookiecutter.project_namespace }}.{{ cookiecutter.project_slug }}'],
     install_requires=requirements,
-    tests_require=['pytest'],
-    setup_requires=setup_requires,
+    tests_require=[],
+    setup_requires=setup_requirements,
     test_suite='tests.python',
     cmake_args=cmake_args,
     cmake_minimum_required_version='3.12',
-
-#[
-#
-#        '-DBUILD_PYTHON_PYBIND11=ON',
-#        '-DBUILD_PYTHON_SWIG=ON',
-#        '-DBUILD_TESTS=ON',
-# to build for a conda environment please check the environment variables
-# https://docs.conda.io/projects/conda-build/en/latest/source/environment-variables.html?highlight=environment
-#        '-DCMAKE_INSTALL_PREFIX={}'.format(os.environ['PREFIX']),
-#        '-DPYTHON_SITE_PACKAGES={}'.format(os.environ['SP_DIR']),
-#    ]
 )
