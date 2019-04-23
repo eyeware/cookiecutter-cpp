45e96762e39507e81f4cee95b0df8858ca5ab300
Mario Costa <mario.silva.costa@gmail.com>
2019-04-23 19:21:34 +0100

    * modified the build to build and install the python bindings * renamed jobs

--- {{cookiecutter.project_slug}}/tests/python/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/python/test_{{cookiecutter.project_slug}}.py
@@ -5,7 +5,7 @@
 
 import unittest
 
-from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
+from {{ cookiecutter.project_namespace }}.{{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
 
 class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
     """Tests for `{{ cookiecutter.project_slug }}` package."""
@@ -18,4 +18,3 @@ class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
 
     def test_000_something(self):
         """Test something."""
-
